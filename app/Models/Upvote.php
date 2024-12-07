4e01898f4c596d029a9f8506e14c963a5e332a8c
sadek1999 <sadekali1530@gmail.com>
2024-12-07 21:05:51 +0600

    set feature ,comment ,upvote table and relations function

---
+++ app/Models/Upvote.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Upvote extends Model
+{
+       use HasFactory;
+       protected $fillable=['upvote','user_id','feature_id'];
+        public function feature()
+        {
+
+        return $this->belongsTo(Feature::class) ;
+        }
+}
