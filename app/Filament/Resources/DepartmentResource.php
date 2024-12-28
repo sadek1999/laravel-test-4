387e9180277ed54a141a098df926960f0f4a22c2
sadek1999 <sadekali1530@gmail.com>
2024-12-28 23:58:35 +0600

    set catagories relation with department

--- app/Filament/Resources/DepartmentResource.php
+++ app/Filament/Resources/DepartmentResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Enum\RolesEnum;
 use App\Filament\Resources\DepartmentResource\Pages;
 use App\Filament\Resources\DepartmentResource\RelationManagers;
+use App\Filament\Resources\DepartmentResource\RelationManagers\CategoriesRelationManager;
 use App\Models\Department;
 use Filament\Facades\Filament;
 use Filament\Forms;
@@ -65,7 +66,7 @@ class DepartmentResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            CategoriesRelationManager::class,
         ];
     }
 
